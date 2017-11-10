94388ba8d95c78e40dbb1475da69e9167c6c5528
Bapusaheb Patil <bapoo1998@gmail.com>
2017-11-10 12:28:29 +0530

    Added placeholder, removed ads

--- app/src/main/java/bapspatil/silverscreener/DetailsActivity.java
+++ app/src/main/java/bapspatil/silverscreener/DetailsActivity.java
@@ -65,7 +65,6 @@ public class DetailsActivity extends AppCompatActivity implements TrailerRecycle
     @BindView(R.id.rv_trailers) MovieRecyclerView mTrailerRecyclerView;
     @BindView(R.id.rv_reviews) MovieRecyclerView mReviewRecyclerView;
     @BindView(R.id.fav_button) FloatingActionButton mFavoriteButton;
-    @BindView(R.id.adview) AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,8 +137,6 @@ public class DetailsActivity extends AppCompatActivity implements TrailerRecycle
 
         (new GetTheTrailersTask()).execute(movie.getId());
         (new GetTheReviewsTask()).execute(movie.getId());
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
     }
 
     @Override
@@ -345,27 +342,4 @@ public class DetailsActivity extends AppCompatActivity implements TrailerRecycle
         Log.d("Remove Fav", "Uri delete: " + uri.toString());
     }
 
-    @Override
-    public void onPause() {
-        if (mAdView != null) {
-            mAdView.pause();
-        }
-        super.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mAdView != null) {
-            mAdView.resume();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        if (mAdView != null) {
-            mAdView.destroy();
-        }
-        super.onDestroy();
-    }
 }
